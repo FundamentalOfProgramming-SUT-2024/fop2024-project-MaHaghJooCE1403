d167bb1f57fa8a3ce92652d31f9456dfc9c6a4ed
MaHaghJooCE1403 <mahaghjoo63@gmail.com>
2024-12-27 18:45:35 +0330

    Start Menus Added

--- Rogue_Prototype.c
+++ Rogue_Prototype.c
@@ -1,10 +1,144 @@
-#include <ncurses.h>
+#include <ncurses.h>    //403110585 Mohammad Amin Haghjoo
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+int ROW,COLUMN;
+void GAME_STARTUP_MENU();
+
+
+
+
+
+
 
 int main() {
     initscr();
-    printw("Hello World!\n");
-    refresh();
+    noecho();
+    keypad(stdscr,TRUE);
+    getmaxyx(stdscr,ROW,COLUMN);
+    if(has_colors()==TRUE) {
+        start_color();
+        init_pair(1,COLOR_YELLOW,COLOR_BLACK);
+        init_pair(2,COLOR_WHITE,COLOR_YELLOW);
+        //add colors later
+
+
+
+
+    }
+
+    GAME_STARTUP_MENU();
+    
+
+
+
+
+
+
+
+
+
+
+
+
+    
     getch();
     endwin();
     return 0;
+}
+
+void GAME_STARTUP_MENU() {
+
+    attron(COLOR_PAIR(1));
+
+    mvprintw(ROW-1,0,"v1.0.0 Developed By Mohammad Amin Haghjoo");
+
+    mvprintw(ROW-1,COLUMN/2-12,"Press e or E to Select a Menu");
+
+    attron(A_ITALIC);
+
+    mvprintw(ROW/4,COLUMN/2 -2,"Rogue");
+
+    attroff(A_ITALIC);
+
+    mvprintw(3*ROW/4,COLUMN/2-8,"Register New User");
+
+    attroff(COLOR_PAIR(1));
+
+    attron(COLOR_PAIR(2) | A_BLINK);
+
+    mvprintw(ROW/2,COLUMN/2 -10 ,"Login or Play as Guest");
+
+    attroff(COLOR_PAIR(2)); attroff(A_BLINK);
+
+    move(ROW/2,COLUMN/2);
+
+    int input;
+
+    do {
+        int x,y;
+
+        getyx(stdscr,y,x);
+
+        input=getch();
+
+        if((input=='e' || input=='E' ) && y==ROW/2) {
+
+            clear();
+
+            break;
+
+        }
+        else if((input=='e' || input=='E') && y==3*ROW/4) {
+            
+            clear();
+
+            break;
+
+        }
+        else if(input==KEY_DOWN && y==ROW/2) {
+
+            attron(COLOR_PAIR(1));
+            
+            mvprintw(ROW/2,COLUMN/2 -10 ,"Login or Play as Guest");
+
+            attroff(COLOR_PAIR(1));
+
+            attron(COLOR_PAIR(2) | A_BLINK);
+
+            mvprintw(3*ROW/4,COLUMN/2-8,"Register New User");
+
+            attroff(COLOR_PAIR(2)); attroff(A_BLINK);
+
+            move(3*ROW/4,COLUMN/2);
+
+        }
+        else if(input==KEY_UP && y==3*ROW/4) {
+
+            attron(COLOR_PAIR(1));
+
+            mvprintw(3*ROW/4,COLUMN/2-8,"Register New User");
+
+            attroff(COLOR_PAIR(1));
+
+            attron(COLOR_PAIR(2) | A_BLINK);
+
+            mvprintw(ROW/2,COLUMN/2 -10 ,"Login or Play as Guest");
+
+            attroff(COLOR_PAIR(2)); attroff(A_BLINK);
+
+            move(ROW/2,COLUMN/2);
+
+        }
+
+        refresh();
+
+    }
+
+    while(TRUE);
+
+
+
+
+
 }
